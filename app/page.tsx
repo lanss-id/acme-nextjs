b9c72ac9e214a4185309aa5a7afce5308cbf4260
Maulana Kayyis <maulanakayyis354@gmail.com>
2023-12-05 16:37:29 +0700

    style(text): delete some text in root page

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
           </p>
           <Link
             href="/login"
